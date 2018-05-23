7949e0fe76d97abad7e05659f53bde835573f805
Dakota Kim <S0281634@SalemState.edu>
2018-05-23 12:31:15 -0400

    UI tweaks. Constraints needed.

---
+++ Pods/Target Support Files/Pods-Timato/Pods-Timato-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Timato : NSObject
+@end
+@implementation PodsDummy_Pods_Timato
+@end
--- Timato/ViewController.swift
+++ Timato/ViewController.swift
@@ -31,7 +31,7 @@ class ViewController: UIViewController {
     ]
     
     let workTime = 1500
-    var workTimeLength = 1500
+    var workTimeLength = 10
     let shortBreakTime = 300
     let longBreakTime = 1800
     
@@ -40,6 +40,7 @@ class ViewController: UIViewController {
     var roundCounter = 1
     
     override func viewDidLoad() {
+        changeColorTheme(color: UIColor.flatRed, lightenedColor: UIColor(hexString: "FF7364")!)
         super.viewDidLoad()
         view.backgroundColor = UIColor.flatRed
         timerLabel.text = String(format: "%02d", (workTimeLength/60)) + ":" + String(format: "%02d", (workTimeLength % 60))
@@ -94,10 +95,35 @@ class ViewController: UIViewController {
     }
     
     func play() {
-        view.backgroundColor = UIColor.flatGreen
+        changeColorTheme(color: UIColor.flatGreen, lightenedColor: UIColor(hexString: "7EE7A8")!)
         timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(ViewController.counter), userInfo: nil, repeats: true)
         playButtonOutlet.isEnabled = false
         stopButtonOutlet.isEnabled = true
+    }
+    
+    func changeColorTheme(color: UIColor, lightenedColor: UIColor) {
+        print(color.hexValue())
+        self.view.backgroundColor = color
+        playButtonOutlet.layer.shadowColor = UIColor.flatBlack.cgColor
+        playButtonOutlet.layer.shadowOffset = CGSize(width: 5.0, height: 2.0)
+        playButtonOutlet.layer.shadowOpacity = 1.0
+        playButtonOutlet.layer.shadowRadius = 5.0
+        playButtonOutlet.layer.masksToBounds = false
+        stopButtonOutlet.layer.shadowColor = UIColor.flatBlack.cgColor
+        stopButtonOutlet.layer.shadowOffset = CGSize(width: 0.0, height: -2.0)
+        stopButtonOutlet.layer.shadowOpacity = 1.0
+        stopButtonOutlet.layer.shadowRadius = 0.0
+        stopButtonOutlet.layer.masksToBounds = false
+        playButtonOutlet.backgroundColor = lightenedColor
+        playButtonOutlet.setTitleColor(ContrastColorOf(color, returnFlat: true), for: .normal)
+        playButtonOutlet.layer.cornerRadius = 12.5
+        playButtonOutlet.clipsToBounds = true
+        stopButtonOutlet.backgroundColor = lightenedColor
+        stopButtonOutlet.setTitleColor(ContrastColorOf(color, returnFlat: true), for: .normal)
+        stopButtonOutlet.layer.cornerRadius = 12.5
+        stopButtonOutlet.clipsToBounds = true
+        inspirationalQuoteLabel.textColor = ContrastColorOf(color, returnFlat: true)
+        timerLabel.textColor = ContrastColorOf(color, returnFlat: true)
     }
 
     
@@ -111,7 +137,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var stopButtonOutlet: UIButton!
     
     @IBAction func stopButtonTapped(_ sender: Any) {
-        view.backgroundColor = UIColor.flatRed
+        changeColorTheme(color: UIColor.flatRed, lightenedColor: UIColor(hexString: "FF7364")!)
         timer.invalidate()
         playButtonOutlet.isEnabled = true
         stopButtonOutlet.isEnabled = true
